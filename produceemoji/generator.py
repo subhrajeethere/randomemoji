1bef5309172a5d67218c8db10db649d3a873b343
Subhrajeet Panda <260500849+subhrajeethere@users.noreply.github.com>
2026-02-28 01:21:22 +0530

    removed 2 static methods

--- produceemoji/generator.py
+++ produceemoji/generator.py
@@ -7,10 +7,3 @@ class RandomEmoji:
   def oneEmoji():
     return random.choice(EMOJI_LIST)
   
-  @staticmethod
-  def many(n = 3):
-    return [random.choice(EMOJI_LIST for _ in range(n))]
-  
-  @staticmethod
-  def string(n = 5, separator = " "):
-    return separator.join(RandomEmoji.many(n))
